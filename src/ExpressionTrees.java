4187a2ca4e2b22e9a7ee71d4a22e1c21c737ac3c
Bob <Bob@DESKTOP-GVDICQ4.fios-router.home>
2018-03-05 01:21:05 -0500

    two - sunday night

--- src/ExpressionTrees.java
+++ src/ExpressionTrees.java
@@ -1,40 +1,69 @@
 
 public class ExpressionTrees implements Expressions {
 
-	
+	/**
+	 * constructor
+	 */
 	public ExpressionTrees() {
 		super();
 	}
 	
-	
+	/**
+	 * creates an expression tree given postifx 
+	 * @param exp
+	 */
 	public TreeNode buildTree(String[] exp) { 
 		//TODO
 		return null;
 	}
 	
+	/**
+	 * returns the result of an expression tree
+	 */
 	public int evalTree() {
 		//TODO
 		return -1;
 	}
 	
+	/**
+	 * converts a tree to prefix notation
+	 */
 	public String toPrefixNotation() {
 		//TODO
 		return null;
 	}
 	
+	/**
+	 * converts a tree to infix notation
+	 */
 	public String toInfixNotation() {
 		//TODO	
 		return null;
 	}
 	
+	/**
+	 * converts a tree to postfix notation
+	 */
 	public String toPostfixNotation() {
 		//TODO		
 		return null;
 	}
 	
+	/**
+	 *evaluates a given postfix expression
+	 * @param exp
+	 */
 	public int postfixEval(String[] exp) {
 		//TODO	
 		return -1;
+	}
+	
+	/**
+	 * tests the program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
 	}
 	
 }
