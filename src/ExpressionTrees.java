7eb3ab2cbc31b22f9dd0c71dc03f0d42e9ad0260
Bob <Bob@DESKTOP-GVDICQ4.fios-router.home>
2018-03-05 23:34:23 -0500

    commit three i have no idea how to build a tree

--- src/ExpressionTrees.java
+++ src/ExpressionTrees.java
@@ -1,15 +1,20 @@
 
-public class ExpressionTrees implements Expressions {
+public class ExpressionTrees extends TreeNode implements Expressions {
 
+	private final String PLUS = "+";
+	private final String MULT = "*";
+	public String[] operators = {PLUS, MULT};
+	
 	/**
 	 * constructor
 	 */
 	public ExpressionTrees() {
-		super();
+		//TODO
+		super(null);
 	}
 	
 	/**
-	 * creates an expression tree given postifx 
+	 * creates an expression tree given postfix 
 	 * @param exp
 	 */
 	public TreeNode buildTree(String[] exp) { 
@@ -20,33 +25,59 @@ public class ExpressionTrees implements Expressions {
 	/**
 	 * returns the result of an expression tree
 	 */
-	public int evalTree() {
-		//TODO
-		return -1;
+	public int evalTree(TreeNode node) {
+		
+		String s = (String) node.getValue();
+		if (isOperator(s)) {
+			if (s.equals(MULT)) 
+				return (evalTree(node.getLeft()) * evalTree(node.getRight()));
+			else 
+				return (evalTree(node.getLeft()) + evalTree(node.getRight()));
+		}
+		else 
+			return Integer.parseInt(s);
+		
 	}
 	
 	/**
 	 * converts a tree to prefix notation
+	 * @param node - root node of the tree being evaluated
 	 */
-	public String toPrefixNotation() {
-		//TODO
-		return null;
+	public String toPrefixNotation(TreeNode node) {
+
+		if (isOperator(node.getValue())) {
+			return ((String) node.getValue() + toPrefixNotation(node.getLeft()) + toPrefixNotation(node.getRight()));
+		}
+		else
+			return (String) node.getValue();
+
 	}
 	
 	/**
 	 * converts a tree to infix notation
+	 * @param node - root node of the tree being evaluated
 	 */
-	public String toInfixNotation() {
-		//TODO	
-		return null;
+	public String toInfixNotation(TreeNode node) {
+		//TODO - no parentheses
+		if (isOperator(node.getValue())) {
+			return (toInfixNotation(node.getLeft()) + (String) node.getValue() + toInfixNotation(node.getRight()));
+		}
+		else
+			return (String) node.getValue();
 	}
 	
 	/**
 	 * converts a tree to postfix notation
+	 * @param node - root node of the tree being evaluated
 	 */
-	public String toPostfixNotation() {
-		//TODO		
-		return null;
+	public String toPostfixNotation(TreeNode node) {
+		
+		if (isOperator(node.getValue())) {
+			return (toPostfixNotation(node.getLeft()) +  toPostfixNotation(node.getRight()) + (String) node.getValue() );
+		}
+		else
+			return (String) node.getValue();
+		
 	}
 	
 	/**
@@ -54,8 +85,26 @@ public class ExpressionTrees implements Expressions {
 	 * @param exp
 	 */
 	public int postfixEval(String[] exp) {
-		//TODO	
-		return -1;
+		return evalTree(buildTree(exp));
+	}
+	
+	/**
+	 * checks if a given character is an operator (in this assignment, only * and +)
+	 * @param o
+	 * @return
+	 */
+	public boolean isOperator (Object o) {
+		
+		return (o.equals(MULT) || o.equals(PLUS));
+		
+	}
+	
+	/**
+	 * 
+	 * @param s
+	 */
+	public static void SOP(String s) {
+		System.out.println(s);
 	}
 	
 	/**
@@ -64,6 +113,26 @@ public class ExpressionTrees implements Expressions {
 	 */
 	public static void main(String[] args) {
 		
+	}
+
+	@Override
+	public int evalTree() {
+		return evalTree(this);
+	}
+
+	@Override
+	public String toPrefixNotation() {
+		return toPrefixNotation(this);
+	}
+
+	@Override
+	public String toInfixNotation() {
+		return toInfixNotation(this);
+	}
+
+	@Override
+	public String toPostfixNotation() {
+		return toPostfixNotation(this);
 	}
 	
 }
